42341900b4044fdfdf6e532ce8188b75d8c5b804
italojsr <italoreis99@gmail.com>
2025-06-02 06:52:42 -0300

    Adicionando seção 4.3 e 4.4

--- src/main/java/br/ufma/Lox.java
+++ src/main/java/br/ufma/Lox.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class Lox {
     static boolean hadError = false; // Flag para indicar se houve um erro
