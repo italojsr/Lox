84be8b6d17ee5f92ae18c2823230334fc4da1061
italojsr <italoreis99@gmail.com>
2025-06-09 06:59:29 -0300

    Adicionando seção 4.5

--- src/main/java/br/ufma/Scanner.java
+++ src/main/java/br/ufma/Scanner.java
@@ -31,8 +31,63 @@ class Scanner {
     return tokens;
   }
 
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(': addToken(TokenType.LEFT_PAREN); break;
+      case ')': addToken(TokenType.RIGHT_PAREN); break;
+      case '{': addToken(TokenType.LEFT_BRACE); break;
+      case '}': addToken(TokenType.RIGHT_BRACE); break;
+      case ',': addToken(TokenType.COMMA); break;
+      case '.': addToken(TokenType.DOT); break;
+      case '-': addToken(TokenType.MINUS); break;
+      case '+': addToken(TokenType.PLUS); break;
+      case ';': addToken(TokenType.SEMICOLON); break;
+      case '*': addToken(TokenType.STAR); break; 
+
+      case '!':
+        addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case '=':
+        addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case '>':
+        addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+        break;
+
+      default:
+        Lox.error(line, "Unexpected character.");
+        break;
+    }
+
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
+  }
+
   private boolean isAtEnd() {
       return current >= source.length();
+  }
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
   }
 
 }
