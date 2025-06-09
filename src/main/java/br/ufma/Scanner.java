c97a8afd52a9d73bd91f504c039eb178786a381e
Hudson Costa <hudsoncdiniz@gmail.com>
2025-06-09 08:08:55 -0300

    feat: Implementar Scanner (até 4.7)

--- src/main/java/br/ufma/Scanner.java
+++ src/main/java/br/ufma/Scanner.java
@@ -1,101 +1,205 @@
-package br.ufma;
-
+package br.ufma; 
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashMap; // Importe HashMap
 import java.util.List;
-import java.util.Map;
+import java.util.Map; // Importe Map
 
-import br.ufma.TokenType.*;
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
+    // Mapa estático para armazenar as palavras reservadas
+    private static final Map<String, TokenType> keywords;
 
-class Scanner {
-  private final String source;
-  private final List<Token> tokens = new ArrayList<>();
-  private int start = 0;
-  private int current = 0;
-  private int line = 1;
-
-  Scanner(String source) {
-    this.source = source;
-  }
-
-  List<Token> scanTokens() {
-    while (!isAtEnd()) {
-        // We are at the beginning of the next lexeme.
-        start = current;
-        scanToken();
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    TokenType.AND);
+        keywords.put("class",  TokenType.CLASS);
+        keywords.put("else",   TokenType.ELSE);
+        keywords.put("false",  TokenType.FALSE);
+        keywords.put("for",    TokenType.FOR);
+        keywords.put("fun",    TokenType.FUN);
+        keywords.put("if",     TokenType.IF);
+        keywords.put("nil",    TokenType.NIL);
+        keywords.put("or",     TokenType.OR);
+        keywords.put("print",  TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super",  TokenType.SUPER);
+        keywords.put("this",   TokenType.THIS);
+        keywords.put("true",   TokenType.TRUE);
+        keywords.put("var",    TokenType.VAR);
+        keywords.put("while",  TokenType.WHILE);
     }
 
-    tokens.add(new Token(TokenType.EOF, "", null, line));
-    return tokens;
-  }
-
-  private void scanToken() {
-    char c = advance();
-    switch (c) {
-      case '(': addToken(TokenType.LEFT_PAREN); break;
-      case ')': addToken(TokenType.RIGHT_PAREN); break;
-      case '{': addToken(TokenType.LEFT_BRACE); break;
-      case '}': addToken(TokenType.RIGHT_BRACE); break;
-      case ',': addToken(TokenType.COMMA); break;
-      case '.': addToken(TokenType.DOT); break;
-      case '-': addToken(TokenType.MINUS); break;
-      case '+': addToken(TokenType.PLUS); break;
-      case ';': addToken(TokenType.SEMICOLON); break;
-      case '*': addToken(TokenType.STAR); break; 
-
-      case '!':
-        addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
-        break;
-      case '=':
-        addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
-        break;
-      case '<':
-        addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
-        break;
-      case '>':
-        addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
-        break;
-
-      default:
-        Lox.error(line, "Unexpected character.");
-        break;
+    public Scanner(String source) {
+        this.source = source;
     }
 
-  }
+    public List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
 
-  private boolean match(char expected) {
-    if (isAtEnd()) return false;
-    if (source.charAt(current) != expected) return false;
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
 
-    current++;
-    return true;
-  }
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
 
-  private boolean isAtEnd() {
-      return current >= source.length();
-  }
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '*': addToken(TokenType.STAR); break;
+            case '!':
+                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+            case '>':
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    // Um comentário vai até o final da linha.
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(TokenType.SLASH);
+                }
+                break;
 
-  private char advance() {
-    return source.charAt(current++);
-  }
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignora espaços em branco.
+                break;
 
-  private void addToken(TokenType type) {
-    addToken(type, null);
-  }
+            case '\n':
+                line++;
+                break;
 
-  private void addToken(TokenType type, Object literal) {
-    String text = source.substring(start, current);
-    tokens.add(new Token(type, text, literal, line));
-  }
+            case '"': string(); break;
 
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier(); // Chama o método identifier para tratar identificadores e palavras reservadas
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
+                break;
+        }
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            br.ufma.Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // O fechamento da aspas.
+        advance();
+
+        // Extrai o valor da string (sem as aspas).
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Verifica a parte fracionária.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consome o '.'.
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER,
+            Double.parseDouble(source.substring(start, current)));
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    // **MÉTODO ATUALIZADO PARA TRATAR IDENTIFICADORES E PALAVRAS RESERVADAS**
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        // Verifica se o lexeme é uma palavra reservada.
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+
+        // Se não for uma palavra reservada, é um identificador.
+        if (type == null) type = TokenType.IDENTIFIER;
+        addToken(type);
+    }
 }
-
-
-
-
-
-
-
-
