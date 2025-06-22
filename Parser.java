df84166d90b9c868f0cb8d89da669e4874e4d0dd
italojsr <italoreis99@gmail.com>
2025-06-22 12:38:49 -0300

    Adicionando métodos da Seção 6.4

--- Parser.java
+++ Parser.java
@@ -13,6 +13,16 @@ class Parser {
     this.tokens = tokens;
   }
 
+  Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
+
+  
+
   private Expr expression() {
     return equality();
   }
@@ -160,6 +170,8 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
+
+    throw error(peek(), "Expect expression.");
   }
 
 
--- src/main/java/br/ufma/Lox.java
+++ src/main/java/br/ufma/Lox.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Lox {
     static boolean hadError = false; // Flag para indicar se houve um erro
@@ -45,9 +46,15 @@ public class Lox {
     }
 
     private static void run(String source) {
-        // Por enquanto, apenas imprime o código fonte.
-        // O scanner será integrado aqui.
-        System.out.println(source);
+        List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+        System.out.println(new AstPrinter().print(expression));
+        
+
     }
 
     // Método para reportar erros
